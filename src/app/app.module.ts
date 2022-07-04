19791cd9c440a028687131b54cd229efa742ffd4
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-04 20:28:25 +0300

    fix auth routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, AuthModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
     providers: [],
     bootstrap: [AppComponent],
 })
