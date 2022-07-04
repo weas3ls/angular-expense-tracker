22d2b0cb9c4bbc636be8ffd039dbc01afc80e233
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-04 17:22:31 +0300

    create auth module and login component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
+];
+
+@NgModule({
+    declarations: [LoginComponent],
+    imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes), MdbFormsModule, MdbValidationModule],
+})
+export class AuthModule {}
