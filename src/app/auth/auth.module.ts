7fe88fe7da52d85b5a9378014386f3d320ea53f4
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-04 21:12:42 +0300

    create register component and functionality

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 import { LoginComponent } from './components/login/login.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
     {
@@ -18,10 +19,14 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent,
     },
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
 ];
 
 @NgModule({
-    declarations: [LoginComponent],
+    declarations: [LoginComponent, RegisterComponent],
     imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes), MdbFormsModule, MdbValidationModule],
 })
 export class AuthModule {}
