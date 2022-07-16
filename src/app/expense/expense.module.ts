1012d1c35c23fedda1e4602096cbf0f7224455b4
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-16 13:14:15 -0500

    create expense-form

--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MdbSelectModule } from 'mdb-angular-ui-kit/select';
+
+import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
+
 @NgModule({
-    declarations: [],
-    imports: [CommonModule],
+    declarations: [ExpenseFormComponent],
+    imports: [CommonModule, MdbSelectModule],
 })
 export class ExpenseModule {}
