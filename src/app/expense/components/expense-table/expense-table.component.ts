9daa4670374b5b48535db391587d32ab9e754910
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-17 17:34:50 -0500

    create expense table, and truncate text pipe

---
+++ src/app/expense/components/expense-table/expense-table.component.ts
@@ -0,0 +1,37 @@
+import { dataSource } from './../../constants/DummyExpenseData';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MdbModalService } from 'mdb-angular-ui-kit/modal';
+import { MdbTableDirective } from 'mdb-angular-ui-kit/table';
+
+import { Expense } from '../../models/expense';
+
+@Component({
+    selector: 'expense-tracker-expense-table',
+    templateUrl: './expense-table.component.html',
+    styleUrls: ['./expense-table.component.scss'],
+})
+export class ExpenseTableComponent implements OnInit {
+    @ViewChild('table') table!: MdbTableDirective<Expense>;
+
+    // modalRef!: MdbModalRef<ExpenseDeleteComponent>;
+
+    constructor(private modalService: MdbModalService) {}
+
+    // openModal() {
+    //     this.modalRef = this.modalService.open(ExpenseDeleteComponent, {
+    //         modalClass: 'modal-dialog-centered modal-sm',
+    //     });
+    // }
+
+    dataSource = dataSource;
+
+    ngOnInit(): void {}
+
+    headers = ['Title', 'Type', 'Date', 'Amount', 'Description', 'Actions'];
+
+    search(event: Event): void {
+        const searchTerm = (event.target as HTMLInputElement).value;
+        this.table.search(searchTerm);
+    }
+}
--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MdbDatepickerModule } from 'mdb-angular-ui-kit/datepicker';
 import { MdbSelectModule } from 'mdb-angular-ui-kit/select';
+import { MdbTableModule } from 'mdb-angular-ui-kit/table';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
+import { SharedModule } from '../shared/shared.module';
 
 import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
 import { ExpenseNewComponent } from './components/expense-new/expense-new.component';
+import { ExpenseTableComponent } from './components/expense-table/expense-table.component';
 
 const routes: Routes = [
     {
@@ -18,7 +23,17 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [ExpenseFormComponent, ExpenseListComponent, ExpenseNewComponent],
-    imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes), MdbSelectModule, MdbValidationModule],
+    declarations: [ExpenseFormComponent, ExpenseListComponent, ExpenseNewComponent, ExpenseTableComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(routes),
+        SharedModule,
+        MdbDatepickerModule,
+        MdbSelectModule,
+        MdbTableModule,
+        MdbValidationModule,
+    ],
 })
 export class ExpenseModule {}
