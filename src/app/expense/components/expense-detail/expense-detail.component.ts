47d15f7f7c880ef7adfa1e54790374d00e8e3ab3
Ahmad Shabaneh <shabaneh.ahmad@gmail.com>
2022-07-17 18:31:46 -0500

    create expense detail component

---
+++ src/app/expense/components/expense-detail/expense-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'expense-tracker-expense-detail',
+    templateUrl: './expense-detail.component.html',
+    styleUrls: ['./expense-detail.component.scss'],
+})
+export class ExpenseDetailComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -4,12 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MdbDatepickerModule } from 'mdb-angular-ui-kit/datepicker';
+import { MdbLightboxModule } from 'mdb-angular-ui-kit/lightbox';
 import { MdbSelectModule } from 'mdb-angular-ui-kit/select';
 import { MdbTableModule } from 'mdb-angular-ui-kit/table';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { ExpenseDetailComponent } from './components/expense-detail/expense-detail.component';
 import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
 import { ExpenseNewComponent } from './components/expense-new/expense-new.component';
@@ -20,10 +23,25 @@ const routes: Routes = [
         path: '',
         component: ExpenseListComponent,
     },
+    {
+        path: ':id',
+        children: [
+            {
+                path: '',
+                component: ExpenseDetailComponent,
+            },
+        ],
+    },
 ];
 
 @NgModule({
-    declarations: [ExpenseFormComponent, ExpenseListComponent, ExpenseNewComponent, ExpenseTableComponent],
+    declarations: [
+        ExpenseFormComponent,
+        ExpenseListComponent,
+        ExpenseNewComponent,
+        ExpenseTableComponent,
+        ExpenseDetailComponent,
+    ],
     imports: [
         CommonModule,
         FormsModule,
@@ -31,8 +49,10 @@ const routes: Routes = [
         RouterModule.forChild(routes),
         SharedModule,
         MdbDatepickerModule,
+        MdbLightboxModule,
         MdbSelectModule,
         MdbTableModule,
+        MdbTooltipModule,
         MdbValidationModule,
     ],
 })
